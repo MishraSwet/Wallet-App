5586c9dec64eb57f95d5b59945f10cfc8c588c74
MishraSwet <swetaparnamishraofficial@gmail.com>
2024-02-19 21:29:19 +0530

    Added Basic Styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import Navigation from './Components/Navigation'
-import Sidebar from './Components/Sidebar'
-import Content from './Components/Content'
+import React from 'react';
+import Navigation from './Components/Navigation';
+import Sidebar from './Components/Sidebar';
+import Content from './Components/Content';
+import Header from './Components/Header';
 
 const App = () => {
   return (
     <>
-      <main>
-        <Navigation />
-        <Content/>
-        <Sidebar/>
+      <Header/>
+      <main style={{display: "flex" , justifyContent:"space-around"}}>
+        <Navigation style={{ flex: "1", minWidth: "25%" }} />
+        <Content style={{ flex: "2", minWidth: "50%" }} />
+        <Sidebar style={{ flex: "1", minWidth: "25%" }} />
       </main>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
