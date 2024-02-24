20033d4b2650c1d6dd1dc2e975ccf1031ddfcbc2
MishraSwet <swetaparnamishraofficial@gmail.com>
2024-02-24 22:51:16 +0530

    Added Header to Layout

--- client/src/Components/Layout.jsx
+++ client/src/Components/Layout.jsx
@@ -1,9 +1,11 @@
+import Header from './Header'
 import React from 'react'
 
 const Layout = ({children}) => {
   return (
     <div className="h-screen bg-black">
       <div className="container h-full mx-auto xl:px-30">
+        <Header/>
         <div className="grid grid-cols-3 h-full">
         {children}       
         </div>
