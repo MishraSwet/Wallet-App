cbbe3b3ee2f0906d37b4117d0ee8e367ea04b618
MishraSwet <swetaparnamishraofficial@gmail.com>
2024-03-20 23:48:11 +0530

    Included Sidebar Component

--- clientv2/src/components/Navbar.jsx
+++ clientv2/src/components/Navbar.jsx
@@ -1,7 +1,9 @@
-export default function Navbar() {
-    return (
-        <div>
-            Navbar Component
-        </div>
-    )
+function Navbar() {
+  return (
+    <div>
+      Navbar
+    </div>
+  )
 }
+
+export default Navbar
